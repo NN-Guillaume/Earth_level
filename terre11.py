5f7768905d82a63871592bb0012bb4c6057da20b
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-04-20 17:46:31 +0200

    the 11th exercise is now working !     :-D

--- terre11.py
+++ terre11.py
@@ -1,17 +1,156 @@
 """ 24 to 12 """
 
+import array as arr
+
 # Créer un programme qui transforme une heure affichée en format 24h au format 12h
 # Attention à midi et minuit.
 
-# the idea is to get this:
-# morning ---> from "10:30" to "10:30 AM"
-# evening ---> from "21:45" to "09:45 AM"
+myHour = int(input())
+myMinute = int(input())
+symbol = ":"
+newTime = [myHour, symbol, myMinute] # working base
+#displayTime = ' '.join([str(elem) for elem in newTime])
+#displayTime = "basic hour print=  %d : %d " % (myHour, myMinute) # will be displayed in the console
 
-# the danger:
-# morning ---> from "12:00" to "12:00 AM"
-# evening ---> from "00:15" to "12:15 AM"
-print("Please, respect the following hour scheme --->   hh:mm   ")
-myHour = input("enter your hour here: ")
+#print(newTime)
+#print(displayTime)
+
+#--------------------------------------------------------------------------------------------------------------------------
+# goodHour was used to build the program
+goodHour = arr.array('i',[
+    00,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23
+])
+
+sillyHour = arr.array('i',[
+    00,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11
+])
+
+#--------------------------------------------------------------------------------------------------------------------------
+""" This part was used to build the program """
+"""
+for x in goodHour :
+    if myHour == goodHour.index(x):
+        #print("goodHour value is matching !") # FONCTIONNE !!! ce print confirme la condition au dessus  :-)
+        pass
+    else:
+        pass
+"""
+
+for x in sillyHour:    
+    if myHour == sillyHour.index(x):
+        #print("sillyHour value is matching !") # ne fonctionne pas à partir de 13h mais detecte deux fois le type 01hxx
+        newTime[0] = sillyHour.index(myHour)
+
+
+    elif myHour != sillyHour.index(x):
+        #print("sillyHour value is NOT matching !") # ne fonctionne pas à partir de 13h mais detecte deux fois le type 01hxx
+
+        if myHour == 13:
+            newTime[0] = sillyHour[13]
+            #print(newTime)
+
+        elif myHour == 14:
+            newTime[0] = sillyHour[14]
+            #print(newTime)
+
+        elif myHour == 15:
+            newTime[0] = sillyHour[15]
+            #print(newTime)
+
+        elif myHour == 16:
+            newTime[0] = sillyHour[16]
+            #print(newTime)
+
+        elif myHour == 17:
+            newTime[0] = sillyHour[17]
+            #print(newTime)
+
+        elif myHour == 18:
+            newTime[0] = sillyHour[18]
+            #print(newTime)
+
+        elif myHour == 19:
+            newTime[0] = sillyHour[19]
+            #print(newTime)
+
+        elif myHour == 20:
+            newTime[0] = sillyHour[20]
+            #print(newTime)
+
+        elif myHour == 21:
+            newTime[0] = sillyHour[21]
+            #print(newTime)
+
+        elif myHour == 22:
+            newTime[0] = sillyHour[22]
+            #print(newTime)
+
+        elif myHour == 23:
+            newTime[0] = sillyHour[23]
+
+
+# to apply AM or PM
+if myHour <= 11:
+    #add index 5 to print "AM"
+    newTime.append(" AM")
+    #pass
+elif myHour >= 12:
+    #add index 5 to print "PM"
+    newTime.append(" PM")
+    #pass
+
+#print(newTime)
+displayTime = ' '.join([str(elem) for elem in newTime])
+print(displayTime)
+
+#--------------------------------------------------------------------------------------------------------------------------
+""" Those parts below were failing attempt to build the program """
 
 """
 goodHour = [
@@ -68,8 +207,8 @@ sillyHour = [
     "23:00 PM"
 ]
 
-hourTable = { goodHour:sillyHour }
-"""
+#hourTable = { goodHour:sillyHour }
+
 hourTable = {
     "00:00" : "00:00 AM",
     "01:00" : "01:00 AM",
@@ -96,12 +235,4 @@ hourTable = {
     "22:00" : "10:00 PM",
     "23:00" : "11:00 PM"
 }
-
-
-for val in hourTable:
-    if myHour == val.keys(): #AttributeError: 'str' object has no attribute 'keys'
-        print("it seems to works up there !")
-        #print(val.keys())
-        #print(val.values())
-        #weirdHour = val.values()
-        #print(weirdHour)
+"""
