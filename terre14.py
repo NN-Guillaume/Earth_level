a8b252038ef6333436e94d244e2afae6e8136c96
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-04-20 21:30:03 +0200

    simple commit

---
+++ terre14.py
@@ -0,0 +1,21 @@
+""" Trier, ou pas ! """
+
+# Créer un programme qui détermine si une liste d'entiers est triée ou non.
+
+#exemples:   
+#  9   8   3        --->    pas triée !
+#  3   8   9  12    --->    triée !
+# Yolooo            --->    nope, erreur !
+
+""" Input the list of values """
+#roundNumberList []
+# I will need to be able to mess with several arguments (no precise number ???) 
+
+""" Condition to determine which value is in the MIDDLE """
+# if the 3 values are the same, gives a error message:
+if "too tired for tonight":
+    "let's do this tomorrow"
+    pass
+
+else:
+    print("oops ! something wet wrong !")
