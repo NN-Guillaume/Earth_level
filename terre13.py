c50ceb06dcf2207d8393476b28cb065ccb6a455e
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-04-20 19:24:04 +0200

    exercise 13 - works !

--- terre13.py
+++ terre13.py
@@ -22,52 +22,23 @@ elif val1 == val2 or val2 == val3 or val1 == val3:
     print("2 options ar the same, can't give the middle number")
 
 # if val1 have the middle value --- WORKS !
-elif val1 >= val2:
-    if val1 <= val3:
-        print(val1)
+elif val1 >= val2 and val1 <= val3:
+    print(val1)
+elif val1 <= val2 and val1 >= val3:
+    print(val1)
+
 
 # if val2 have the middle value --- DOESN'T WORKS !
-elif val2 >= val3:
-    if val2 <= val1:
-        print(val2)
+elif val2 >= val3 and val2 <= val1:
+    print(val2)
+elif val2 <= val3 and val2 >= val1:
+    print(val2)
 
 # if val3 have the middle value --- DOESN'T WORKS !
-elif val3 >= val1:
-    if val3 <= val2:
-        print(val3)
+elif val3 >= val1 and val3 <= val2:
+    print(val3)
+elif val3 <= val1 and val3 >= val2:
+    print(val3)
 
 else:
     print("oops ! something wet wrong !")
-
-
-
-""" Condition to determine which value is the GREATER """
-
-"""
-# if the 3 values are the same, gives a error message:
-if val1 == val2 and val2 == val3 :
-    print("you have put the same number 3 times.")
-
-# ???
-elif val1 == val2:
-    if val3 >= val1:
-        print(val3)
-    else:
-        print(val1)
-
-# ???
-elif val1 == val3:
-    if val2 >= val1:
-        print(val2)
-    else:
-        print(val1)
-
-# ???
-elif val1 >= val2:
-    if val1 >= val3:
-        print(val1)
-    elif val1 <= val3:
-        print(val2)
-    else:
-        print(val3)
-"""
