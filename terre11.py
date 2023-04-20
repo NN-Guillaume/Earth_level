e7d2cc234402b5cd8efca94f0109f7d900f023f5
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-04-20 18:30:34 +0200

    exercise 12 is also working.

--- terre11.py
+++ terre11.py
@@ -8,12 +8,7 @@ import array as arr
 myHour = int(input())
 myMinute = int(input())
 symbol = ":"
-newTime = [myHour, symbol, myMinute] # working base
-#displayTime = ' '.join([str(elem) for elem in newTime])
-#displayTime = "basic hour print=  %d : %d " % (myHour, myMinute) # will be displayed in the console
-
-#print(newTime)
-#print(displayTime)
+newTime = [myHour, symbol, myMinute] # that is the working base
 
 #--------------------------------------------------------------------------------------------------------------------------
 # goodHour was used to build the program
@@ -81,7 +76,9 @@ for x in goodHour :
     else:
         pass
 """
+#--------------------------------------------------------------------------------------------------------------------------
 
+""" This part analyse the input and convert the hour (only the number) """
 for x in sillyHour:    
     if myHour == sillyHour.index(x):
         #print("sillyHour value is matching !") # ne fonctionne pas à partir de 13h mais detecte deux fois le type 01hxx
@@ -91,63 +88,60 @@ for x in sillyHour:
     elif myHour != sillyHour.index(x):
         #print("sillyHour value is NOT matching !") # ne fonctionne pas à partir de 13h mais detecte deux fois le type 01hxx
 
+        # haven't found how to simplify this part...  :-(
         if myHour == 13:
             newTime[0] = sillyHour[13]
-            #print(newTime)
 
         elif myHour == 14:
             newTime[0] = sillyHour[14]
-            #print(newTime)
 
         elif myHour == 15:
             newTime[0] = sillyHour[15]
-            #print(newTime)
 
         elif myHour == 16:
             newTime[0] = sillyHour[16]
-            #print(newTime)
 
         elif myHour == 17:
             newTime[0] = sillyHour[17]
-            #print(newTime)
 
         elif myHour == 18:
             newTime[0] = sillyHour[18]
-            #print(newTime)
 
         elif myHour == 19:
             newTime[0] = sillyHour[19]
-            #print(newTime)
 
         elif myHour == 20:
             newTime[0] = sillyHour[20]
-            #print(newTime)
 
         elif myHour == 21:
             newTime[0] = sillyHour[21]
-            #print(newTime)
 
         elif myHour == 22:
             newTime[0] = sillyHour[22]
-            #print(newTime)
 
         elif myHour == 23:
             newTime[0] = sillyHour[23]
 
 
-# to apply AM or PM
+""" This part of the code here define the AM or PM """
 if myHour <= 11:
-    #add index 5 to print "AM"
+    #add index[5] to print "AM"
     newTime.append(" AM")
-    #pass
 elif myHour >= 12:
-    #add index 5 to print "PM"
+    #add index[5] to print "PM"
     newTime.append(" PM")
-    #pass
 
-#print(newTime)
+
+""" Display the final result ! """
+# turn the array into a string, more eye-friendly to read.
 displayTime = ' '.join([str(elem) for elem in newTime])
+# after all that mess, this display the converted hour into the console
 print(displayTime)
+# end !
+
+# The code is working BUT...
+#... I could implement some user imput verification...
+#... an make a sacrific by a fullmoon night to know how to automate some part of the code TT___TT
 
 #--------------------------------------------------------------------------------------------------------------------------
 """ Those parts below were failing attempt to build the program """
--- terre12.py
+++ terre12.py
@@ -2,3 +2,115 @@
 
 # Créer un programme qui transforme une heure affichée en format 12h au format 24h
 # Attention à midi et minuit.
+
+import array as arr
+
+myHour = int(input())
+myMinute = int(input())
+symbol = ":"
+indicator = input() # automate this to define if it is AM or PM ?
+sillyGrade = indicator.upper()
+newTime = [myHour, symbol, myMinute, sillyGrade ] # that is the working base
+
+#--------------------------------------------------------------------------------------------------------------------------
+# goodHour was used to build the program
+morningHours = arr.array('i',[
+    00,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11
+])
+
+eveningHours = arr.array('i',[
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23
+])
+
+
+""" This part analyse the input and convert the hour (only the number) """
+
+if sillyGrade == "AM":
+    for x in morningHours:
+        if myHour == morningHours.index(x):
+            newTime[0] = morningHours.index(myHour)
+
+if sillyGrade == "PM":
+    for x in eveningHours:
+
+        if myHour != eveningHours.index(x):
+
+            if myHour == 12:
+                newTime[0] = eveningHours[0]
+
+            if myHour == 1:
+                newTime[0] = eveningHours[1]
+
+            if myHour == 2:
+                newTime[0] = eveningHours[2]
+
+            if myHour == 3:
+                newTime[0] = eveningHours[3]
+
+            if myHour == 4:
+                newTime[0] = eveningHours[4]
+
+            if myHour == 5:
+                newTime[0] = eveningHours[5]
+
+            if myHour == 6:
+                newTime[0] = eveningHours[6]
+
+            if myHour == 7:
+                newTime[0] = eveningHours[7]
+
+            if myHour == 8:
+                newTime[0] = eveningHours[8]
+
+            if myHour == 9:
+                newTime[0] = eveningHours[9]
+
+            if myHour == 10:
+                newTime[0] = eveningHours[10]
+
+            if myHour == 11:
+                newTime[0] = eveningHours[11]
+
+
+""" This part of the code here define the AM or PM """
+
+if sillyGrade == "AM":
+    #remove AM
+    newTime.pop(3)
+elif sillyGrade == "PM":
+    #remove PM
+    newTime.pop(3)
+
+
+""" Display the final result ! """
+# turn the array into a string, more eye-friendly to read.
+displayTime = ' '.join([str(elem) for elem in newTime])
+# after all that mess, this display the converted hour into the console
+print(displayTime)
+# end !
+
+# The code is working BUT...
+#... I could implement some user imput verification...
+#... an make a sacrific by a fullmoon night to know how to automate some part of the code TT___TT
